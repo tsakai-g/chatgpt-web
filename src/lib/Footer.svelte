359271a86bf30af3128046cc94b7aadcf462c55e
Tomoya Sakai <102935520+tsakai-g@users.noreply.github.com>
2023-03-08 21:00:19 +0900

    Update Footer.svelte

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -3,10 +3,11 @@
     <p>
       <strong>ChatGPT-web</strong>
       by
-      <a href="https://github.com/tsakai-g/">tsakai</a>
+      <a href="https://niekvandermaas.nl/">Niek van der Maas</a>
+      modified by <a href="https://github.com/tsakai-g/">tsakai</a>
       &mdash; see
-      <a href="https://github.com/tsakai-g/chatgpt-web">GitHub</a>
-      for source code.
+      <a href="https://github.com/Niek/chatgpt-web">GitHub</a>      for original and 
+      <a href="https://github.com/tsakai-g/chatgpt-web">GitHub</a>      for modified source code.
     </p>
   </div>
 </footer>
